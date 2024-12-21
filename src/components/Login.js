a1ef1a3b97aea8327feed7be289d8bde07ba1f5b
rumesh <rumeshk066@gmail.com>
2024-12-21 22:50:39 +0530

    add bg

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Login = () => {
+    return (
+        <>
+          <img src="./img/bg.jpg" alt='background' />
+        </>
+    )
+}
+
+export default Login;
