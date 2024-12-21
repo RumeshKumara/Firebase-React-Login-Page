a9b37cbd2f68c698a5b6254f763032c537938508
rumesh <rumeshk066@gmail.com>
2024-12-21 23:24:47 +0530

    commit-2

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,47 @@ import styled from 'styled-components';
 const Login = () => {
     return (
         <>
-          <img src="./img/bg.jpg" alt='background' />
+          <LoginContainer>
+            <BackImg>
+               <img src="./img/bg.jpg" alt='background'/>
+            </BackImg>
+
+            <LoginForm>
+              <input type="text" placeholder="Username"/>
+              <input type="password" placeholder="Password"/>
+              <a href="/">fogot password?</a>
+              <LoginButton>
+                <button type="submit">Login</button>
+              </LoginButton>
+            </LoginForm>
+          </LoginContainer>
         </>
     )
 }
 
 export default Login;
+
+const LoginContainer = styled.div`
+overflow: hidden;
+  `;
+
+  const BackImg = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  z-index: -1;
+  `;
+  
+  const LoginForm = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: center;
+  align-items: center;
+
+  `;
+
+  const LoginButton = styled.div`
+  position: absolute;
+  `;
